36cfc1de4375507bf2e4f05b45a4ea4c6dd3040f
rails <rails@rails-dev.(none)>
2014-01-20 17:27:37 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SalaryManagementSystem::Application.config.secret_key_base = 'addc63d398ab1cdd34d466b1386cdc760e15b932f34a981abe15379f9139f8d054b366aed11ea9554b0b43295420fad2f4e75969002440268b544428986afa43'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SalaryManagementSystem::Application.config.session_store :cookie_store, key: '_salary_management_system_session'
